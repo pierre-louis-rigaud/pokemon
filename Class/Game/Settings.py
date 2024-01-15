b9ab47cb770c0e838527ae7943b089147ab28e07
CoinFLip2 <pierrelouis3131@gmail.com>
2024-01-15 16:06:51 +0100

    combat pokemon actif (manque système de dégat enf onction du type, choix du combat, RUN, autre pokémon à choisir)

--- Class/Game/Settings.py
+++ Class/Game/Settings.py
@@ -11,19 +11,19 @@ class Settings:
         self.language = "english"
 
         # path to pokemon data #
-        with open("../../Data/Pokemon/Pokedex.json", "r", encoding="utf8") as json_file:
+        with open("Data/Pokemon/Pokedex.json", "r", encoding="utf8") as json_file:
             self.pokedex = json.load(json_file)
 
         # path to type chart #
-        with open("../../Data/Pokemon/Type_chart.json", "r", encoding="utf8") as json_file:
+        with open("Data/Pokemon/Type_chart.json", "r", encoding="utf8") as json_file:
             self.type_chart = json.load(json_file)
 
         # path to natures #
-        with open("../../Data/Pokemon/Nature.json", "r", encoding="utf8") as json_file:
+        with open("Data/Pokemon/Nature.json", "r", encoding="utf8") as json_file:
             self.natures = json.load(json_file)
 
         # path to moves #
-        with open("../../Data/Pokemon/Moves.json", "r", encoding="utf8") as json_file:
+        with open("Data/Pokemon/Moves.json", "r", encoding="utf8") as json_file:
             self.moves = json.load(json_file)
 
         # pygame settings #
@@ -31,7 +31,7 @@ class Settings:
         self.FPS = 60
 
         # Fonts #
-        self.combat_font = ft.Font("../../Data/Game/Font/pokemon_fire_red.ttf", 32)
+        self.combat_font = ft.Font("Data/Game/Font/pokemon_fire_red.ttf", 32)
 
 
 class Generate(Settings):
@@ -83,27 +83,27 @@ class Sprites(Colors):
     def __init__(self):
         super().__init__()
         # Combat Sprites #
-        self.forest_background = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/Background/Forest_Background.png"), (1280, 500))
-        self.desert_background = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/Background/Desert_Background.png"), (1280, 500))
-        self.lake_background = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/Background/Lake_Background.png"), (1280, 500))
-        self.sea_background = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/Background/Sea_Background.png"), (1280, 500))
-        self.training_background = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/Background/Training_Background.png"), (1280, 500))
+        self.forest_background = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/Background/Forest_Background.png"), (1280, 500))
+        self.desert_background = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/Background/Desert_Background.png"), (1280, 500))
+        self.lake_background = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/Background/Lake_Background.png"), (1280, 500))
+        self.sea_background = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/Background/Sea_Background.png"), (1280, 500))
+        self.training_background = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/Background/Training_Background.png"), (1280, 500))
 
         # UI Sprites #
-        self.bottom_message_box = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Bottom_Message_Box.png"), (1280, 220))
-        self.choice_box = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Choice_Box.png"), (600, 220))
-        self.choice_arrow = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Choice_Arrow.png"), (40, 50))
-        self.enemy_pokemon_status = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Enemy_Pokemon_Stats.png"), (500, 150))
-        self.player_pokemon_status = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Player_Pokemon_Stats.png"), (500, 150))
-        self.choice_move_box = pg.transform.scale(pg.image.load("../../Data/Combat/Combat_Sprite/CombatUI/Move_Box.png"), (1280, 220))
+        self.bottom_message_box = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Bottom_Message_Box.png"), (1280, 220))
+        self.choice_box = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Choice_Box.png"), (600, 220))
+        self.choice_arrow = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Choice_Arrow.png"), (40, 50))
+        self.enemy_pokemon_status = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Enemy_Pokemon_Stats.png"), (500, 150))
+        self.player_pokemon_status = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Player_Pokemon_Stats.png"), (500, 150))
+        self.choice_move_box = pg.transform.scale(pg.image.load("Data/Combat/Combat_Sprite/CombatUI/Move_Box.png"), (1280, 220))
 
         # Animation Cooldown #
         self.player_intro_speed = 250
 
     def get_pokemon_sprite(self, pokemon_id, position):
-        sprite = pg.image.load(f"../../Data/Pokemon/Pokemon_Sprites/{position}/{pokemon_id}.png").convert_alpha()
+        sprite = pg.image.load(f"Data/Pokemon/Pokemon_Sprites/{position}/{pokemon_id}.png").convert_alpha()
         return pg.transform.scale(sprite, (300, 300))
 
     def get_combat_player_sprite(self, id):
-        sprite = pg.image.load(f"../../Data/Combat/Combat_Intro_Player/{id}.png")
+        sprite = pg.image.load(f"Data/Combat/Combat_Intro_Player/{id}.png")
         return pg.transform.scale(sprite, (sprite.get_width() * 6, sprite.get_height() * 6))
